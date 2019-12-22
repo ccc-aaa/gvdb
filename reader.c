36af785ce80b573c85b1bc9b34abaf1f2d070e38
Chris Barts <chbarts@gmail.com>
2019-12-22 01:05:26 -0700

    Improve reader.c

--- reader.c
+++ reader.c
@@ -1,56 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include "gvdb-reader.h"
 
 int main(int argc, char *argv[])
 {
-    struct stat statbuf;
     GvdbTable *table;
-    GBytes *bytes;
-    GError *error;
+    GError *error = NULL;
     gchar **names;
     gsize length, n;
-    size_t flen;
-    int fd, i;
+    int i;
 
     for (i = 1; i < argc; i++) {
-        if ((fd = open(argv[i], O_RDONLY)) == -1) {
-            perror(argv[i]);
-            continue;
-        }
-
-        if (fstat(fd, &statbuf) == -1) {
-            perror(argv[i]);
-            close(fd);
-            continue;
-        }
-
-        if ((bytes =
-             (GBytes *) mmap(NULL, (size_t) statbuf.st_size, PROT_READ,
-                             MAP_PRIVATE, fd, 0)) == MAP_FAILED) {
-            perror(argv[i]);
-            close(fd);
+        if ((table = gvdb_table_new(argv[i], TRUE, &error)) == NULL) {
+            fprintf(stderr, "Can't read database from %s: %s\n", argv[i],
+                    error->message);
+            g_error_free(error);
+            error = NULL;
             continue;
         }
 
         puts(argv[i]);
 
-        if ((table =
-             gvdb_table_new_from_bytes(bytes, TRUE, &error)) == NULL) {
-            munmap(bytes, (size_t) statbuf.st_size);
-            close(fd);
-            continue;
-        }
-
         if ((names = gvdb_table_get_names(table, &length)) == NULL) {
             g_free(table);
-            munmap(bytes, (size_t) statbuf.st_size);
-            close(fd);
             continue;
         }
 
@@ -62,8 +34,6 @@ int main(int argc, char *argv[])
 
         g_free(names);
         g_free(table);
-        munmap(bytes, (size_t) statbuf.st_size);
-        close(fd);
     }
 
     return 0;
