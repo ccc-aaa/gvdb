7ac763d770a810074bc353acc12ac72e15d629ee
Chris Barts <chbarts@gmail.com>
2019-12-22 03:04:34 -0700

    Refactor

--- reader.c
+++ reader.c
@@ -21,6 +21,8 @@
 #include <stdlib.h>
 #include "gvdb-reader.h"
 
+#define DEPTH 4
+
 static void recurse_table(GvdbTable * table, int depth)
 {
     GvdbTable *rtable;
@@ -38,12 +40,12 @@ static void recurse_table(GvdbTable * table, int depth)
         if (gvdb_table_has_value(table, names[n]) == TRUE) {
             var = gvdb_table_get_value(table, names[n]);
             val = g_variant_print_string(var, NULL, TRUE);
-            printf("%*c%s\n", depth + 4, ' ', val->str);
+            printf("%*c%s\n", depth + DEPTH, ' ', val->str);
             g_string_free(val, TRUE);
             g_variant_unref(var);
         } else if ((rtable = gvdb_table_get_table(table, names[n])) !=
                    NULL) {
-            recurse_table(rtable, depth + 4);
+            recurse_table(rtable, depth + DEPTH);
             gvdb_table_free(rtable);
         }
     }
@@ -53,12 +55,8 @@ static void recurse_table(GvdbTable * table, int depth)
 
 int main(int argc, char *argv[])
 {
-    GvdbTable *table, *rtable;
-    GVariant *var;
     GError *error = NULL;
-    GString *val;
-    gchar **names;
-    gsize length, n;
+    GvdbTable *table;
     int i;
 
     for (i = 1; i < argc; i++) {
@@ -72,29 +70,10 @@ int main(int argc, char *argv[])
 
         puts(argv[i]);
 
-        if ((names = gvdb_table_get_names(table, &length)) == NULL) {
-            g_free(table);
-            continue;
-        }
-
-        for (n = 0; n < length; n++) {
-            printf("%*c%s\n", 4, ' ', names[n]);
-            if (gvdb_table_has_value(table, names[n]) == TRUE) {
-                var = gvdb_table_get_value(table, names[n]);
-                val = g_variant_print_string(var, NULL, TRUE);
-                printf("%*c%s\n", 8, ' ', val->str);
-                g_string_free(val, TRUE);
-                g_variant_unref(var);
-            } else if ((rtable = gvdb_table_get_table(table, names[n])) !=
-                       NULL) {
-                recurse_table(rtable, 8);
-                gvdb_table_free(rtable);
-            }
-        }
+        recurse_table(table, DEPTH);
 
         puts("");
 
-        g_free(names);
         gvdb_table_free(table);
     }
 
