a3b6258cdd8afbbe6206b78ec1e4ab2d271801e4
a12092 <ccchen@aaasec.com.tw>
2024-10-28 16:41:59 +0800

    Listing instead of indentation.

--- reader.c
+++ reader.c
@@ -23,33 +23,45 @@
 
 #define DEPTH 4
 
-static void recurse_table(GvdbTable * table, int depth)
+static void recurse_table(GvdbTable * table, int depth, const gchar* prefix)
 {
     GvdbTable *rtable;
     GVariant *var;
     GString *val;
     gchar **names;
-    gsize length, n;
+    gsize length;
 
     if ((names = gvdb_table_get_names(table, &length)) == NULL) {
         return;
     }
 
-    for (n = 0; n < length; n++) {
-        printf("%*c%s\n", depth, ' ', names[n]);
+    for (gsize n = 0; n < length; n++) {
         if (gvdb_table_has_value(table, names[n]) == TRUE) {
+            printf("%*c", depth, ' ');
+            if(prefix != NULL) { printf("%s ", prefix); }
+            printf("%s:", names[n]);
             var = gvdb_table_get_value(table, names[n]);
             val = g_variant_print_string(var, NULL, TRUE);
-            printf("%*c%s\n", depth + DEPTH, ' ', val->str);
+            printf("\t%s\n", val->str);
             g_string_free(val, TRUE);
             g_variant_unref(var);
         } else if ((rtable = gvdb_table_get_table(table, names[n])) !=
                    NULL) {
-            recurse_table(rtable, depth + DEPTH);
+            gsize limit = strlen(names[n])+1;
+            if(prefix != NULL) { limit += strlen(prefix)+1; }
+            gchar* buffer = g_new0(gchar, limit);
+            if(prefix != NULL) {
+              strncpy(buffer, prefix, limit);
+              strncat(buffer, " ", limit - strlen(buffer));
+            }
+            strncat(buffer, names[n], limit - strlen(buffer));
+            recurse_table(rtable, depth, buffer);
+            free(buffer);
             gvdb_table_free(rtable);
         }
     }
 
+    for (gsize n = 0; n < length; n++) { g_free(names[n]); }
     g_free(names);
 }
 
@@ -70,7 +82,7 @@ int main(int argc, char *argv[])
 
         puts(argv[i]);
 
-        recurse_table(table, DEPTH);
+        recurse_table(table, DEPTH, NULL);
 
         puts("");
 
